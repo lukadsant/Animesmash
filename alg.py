d62772a55f9140e41eaaa09dadeed2c50084af7e
lucas <lucasbdsaint@gmail.com>
2023-09-05 00:40:12 -0300

    classe Carta

--- alg.py
+++ alg.py
@@ -5,43 +5,110 @@ totalpoints=0
 arenaA=[]
 arenaA2=[]
 
+#ao revelar
+#constante
+#sem habilidade
+#descartar
+#mover
+#destruir
 
+class Carta:
+    def __init__(self,name,poder, custo,habilidade):
+        self.nome=name
+        self.poder=poder
+        self.custo=custo
+        self.habilidade=habilidade
+    
+    def __str__(self):
+        return f"Nome {self.nome}, Poder: {self.poder}, Custo: {self.custo}, Habilidade: {self.habilidade}"
+    
 
-def ability(card):
+minha_carta = Carta("Goku", 3, 3, "destroy")
+print(minha_carta.poder)
+
+def ability(card,player):
     
     escolhas=['buff','debuff','destroy','clone']
-    case=random.choice(escolhas)
+    if player ==1:
+        case=card.habilidade
+    else:
+        case=random.choice(escolhas)
+    
     print(f'efeito da carta: {case}')
+    
     if case=='buff':
         cardbuff=card
-        arenaA[-1]=cardbuff+3
+        if player==1:
+            arenaA[-1].poder=cardbuff.poder+3
+        else:
+            arenaA2[-1].poder=cardbuff.poder+3
     elif case=='debuff':
-        enemy=arenaA2[-1]
-        arenaA2[-1]=enemy-1
+        if player==1:
+            enemy=arenaA2[-1].poder
+            arenaA2[-1].poder=enemy-1
+        else:
+            enemy=arenaA[-1].poder
+            arenaA[-1].poder=enemy-1
     elif case == 'destroy':
-        arenaA2.pop()
+        if player==1:
+            if len(arenaA2) > 0:
+                arenaA2.pop()
+            else:
+                print("A lista arenaA2 está vazia. Não é possível fazer o pop.")
+        else:
+            if len(arenaA) > 0:
+                arenaA.pop()
+            else:
+                print("A lista arenaA está vazia. Não é possível fazer o pop.")
+            
     elif case == 'clone':
-        arenaA.append(card)
-
+        if player==1:
+            arenaA.append(card)
+        else:
+            arenaA2.append(card)
         
     
 while turn < 6:
-    print("Turn", turn)
+    print(f"Turno {turn}\n")
     
     card=int(input('Insira seu card '))
     
     
-    arenaA.append(card)
-    arenaA2.append(2)
-    print(f'player: {arenaA}')
-    print(f'bot: {arenaA2}')
-    print('ativando efeito da carta....')
-
-    ability(arenaA[-1])
+    arenaA.append(minha_carta)
+    arenaA2.append(minha_carta)
     print(f'player: {arenaA}')
     print(f'bot: {arenaA2}')
     
-    totalpoints=sum(arenaA)
+    if len(arenaA) > 0:
+        print('ativando efeito da carta do player....')
+        ability(arenaA[-1],1)
+    else:
+        print('player sem carta')
+    if len(arenaA2) > 0:
+        print('ativando efeito da carta do bot....')
+        ability(arenaA2[-1],2)
+    else:
+        print('bot sem carta')
+    # Para o jogador
+    print("Player:")
+    for carta in arenaA:
+        print(f'Nome: {carta.nome}, Poder: {carta.poder}')
+
+    # Para o bot
+    print("Bot:")
+    for carta in arenaA2:
+        print(f'Nome: {carta.nome}, Poder: {carta.poder}')
+    
+    playertotalpoints=sum(carta.poder for carta in arenaA)
+    bottotalpoints=sum(carta.poder for carta in arenaA2)
     turn=turn+1
     
-print("Total points:", totalpoints, "Points")
+print("Player Total points:", playertotalpoints, "Points")
+print("Bot Total points:", bottotalpoints, "Points")
+
+if playertotalpoints > bottotalpoints:
+    print("O jogador é o vencedor!")
+elif playertotalpoints < bottotalpoints:
+    print("O bot é o vencedor!")
+else:
+    print("É um empate!")
